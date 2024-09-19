c559b8e5b502408ef6541762ed01eda1737a7af1
Charlie131 <charlieeelages@gmail.com>
2024-09-18 23:20:07 -0300

    fix: fixed product generation

--- src/product.ts
+++ src/product.ts
@@ -6,40 +6,50 @@ export interface ProductImageSource {
 }
 export interface ProductData {
     imageSrc: ProductImageSource;
+    category: string;
     name: string;
-    description: string;
     price: number;
     quantity: number;
     enviroment: keyof ProductImageSource;
-
 }
 export class ProductComponent {
     private element: HTMLElement;
+    private container: HTMLElement;
     private data: ProductData;
 
     constructor(selector: string, data: ProductData){
-        this.element = document.querySelector(selector) as HTMLElement;
+        this.container = document.querySelector(selector) as HTMLElement;
+        this.element = document.createElement("div");
+        this.element.classList.add("product");
         this.data = data;
         this.render();
+        this.container.appendChild(this.element);
     }
     
     render(){
-        const { imageSrc, name, description, price, quantity, enviroment } = this.data;
+        const { imageSrc, category, name, price, quantity, enviroment } = this.data;
         this.element.innerHTML = `
-      <div class="product">
-        <img class="thumb selected-${quantity!=0}" src="${imageSrc[enviroment]}" />
+        <img class="thumb selected-${quantity>0}" src="${imageSrc[enviroment]}" />
         <div class="btn-container">
-          <button class="butt cart-butt">
+          <button class="butt cart-butt ${quantity>0 ? 'hidden' : ''}">
             <img class="cart-butt-icon" src="/src/assets/images/icon-add-to-cart.svg" />
             <span>Add to Cart</span>
           </button>
+          <div class="butt quantity-controller ${quantity<=0 ? 'hidden' : ''}">
+            <button class="butt ico-butt decrement">
+              <img class="ico-butt-ico" src="/src/assets/images/icon-decrement-quantity.svg" />
+            </button>
+            ${quantity}
+            <button class="butt ico-butt increment">
+              <img class="ico-butt-ico" src="/src/assets/images/icon-increment-quantity.svg" />
+            </button>
+          </div>
         </div> 
         <div class="data">
+          <span class="category">${category}</span>
           <span class="name">${name}</span>
-          <span class="desc">${description}</span>
           <span class="price">${price}</span>
         </div>
-      </div>
         `;
         
         this.addEventListeners();
@@ -47,13 +57,26 @@ export class ProductComponent {
 
     addEventListeners(){
         const button = this.element.querySelector('.cart-butt');
+        const dec = this.element.querySelector('.decrement');
+        const inc = this.element.querySelector('.increment');
         button?.addEventListener('click', this.handleAddToCart.bind(this));
+        dec?.addEventListener('click', this.handleDecrement.bind(this));
+        inc?.addEventListener('click', this.handleIncrement.bind(this));
     }
 
     handleAddToCart() {
-        alert(`${this.data.name} added to cart!`);
+        this.data.quantity = 1;
+        this.render();
     }
 
+    handleDecrement(){
+      this.data.quantity -= 1;
+      this.render();
+    }
 
+    handleIncrement(){
+      this.data.quantity += 1;
+      this.render();
+    }
     
 }
